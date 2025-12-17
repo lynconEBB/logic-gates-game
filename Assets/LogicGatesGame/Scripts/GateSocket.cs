2183128ed11f56625c9934e97f26929db6c3260e
Lyncon Baez <lyncon.baez@pti.org.br>
2025-12-17 09:33:45 -0300

    All gate prefabs

---
+++ Assets/LogicGatesGame/Scripts/GateSocket.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+
+namespace LogicGatesGame.Scripts
+{
+    public class GateSocket : XRSocketInteractor
+    {
+         
+        
+    }
+}
